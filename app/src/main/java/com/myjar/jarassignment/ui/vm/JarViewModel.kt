91bd66e4eba844f8857fe04ed585c0e4ea4e7386
harshit.chauhan <harshit.chauhan@blu-smart.com>
2024-12-24 12:08:03 +0530

    [ADDED]: Added Search Functionality

--- app/src/main/java/com/myjar/jarassignment/ui/vm/JarViewModel.kt
+++ app/src/main/java/com/myjar/jarassignment/ui/vm/JarViewModel.kt
@@ -6,11 +6,18 @@ import com.myjar.jarassignment.createRetrofit
 import com.myjar.jarassignment.data.model.ComputerItem
 import com.myjar.jarassignment.data.repository.JarRepository
 import com.myjar.jarassignment.data.repository.JarRepositoryImpl
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 
+@OptIn(FlowPreview::class)
 class JarViewModel : ViewModel() {
 
     private val _listStringData = MutableStateFlow<List<ComputerItem>>(emptyList())
@@ -19,11 +26,39 @@ class JarViewModel : ViewModel() {
 
     private val repository: JarRepository = JarRepositoryImpl(createRetrofit())
 
+    private val _isSearching = MutableStateFlow(false)
+    val isSearching: StateFlow<Boolean>
+        get() = _isSearching
+
+    private val _searchTerm = MutableStateFlow("")
+    val searchTerm: StateFlow<String>
+        get() = _searchTerm
+
+    fun updateSearchTerm(searchTerm: String) {
+        _searchTerm.value = searchTerm
+    }
+
+    val filteredList = searchTerm
+        .debounce(500)
+        .onEach { _isSearching.value = true }
+        .combine(_listStringData) { text, searchList ->
+            if (text.isEmpty()) {
+                searchList
+            } else {
+                delay(2000)
+                searchList.filter {
+                    it.name.contains(text, ignoreCase = true)
+                }
+            }
+        }.onEach {
+            _isSearching.value = false
+        }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), _listStringData.value)
+
     fun fetchData() {
         viewModelScope.launch {
-           repository.fetchResults().collectLatest { resultList ->
-               _listStringData.value = resultList
-           }
+            repository.fetchResults().collect { resultList ->
+                _listStringData.value = resultList
+            }
         }
     }
 }
